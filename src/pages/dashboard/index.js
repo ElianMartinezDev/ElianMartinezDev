b0a01e2de2f6388a795a6e568f5d5b06eac59eb4
ElianMartinezDev <elian.martinez@grupobabel.com>
2022-04-21 01:44:06 -0400

    fix login

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+// next
+import { useRouter } from 'next/router';
+// config
+import { PATH_AFTER_LOGIN } from '../../config';
+// routes
+
+// ----------------------------------------------------------------------
+
+export default function Index() {
+  const { pathname, replace, prefetch } = useRouter();
+  console.log(pathname);
+  useEffect(() => {
+    if (pathname === '/') {
+      replace(PATH_AFTER_LOGIN);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname]);
+
+  useEffect(() => {
+    prefetch(PATH_AFTER_LOGIN);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return null;
+}
